59c54fbb946bf357b6269a0c2ffb906ae1593b73
OLUFEMIADEWOYIN <147109731+OLUFEMIADEWOYIN@users.noreply.github.com>
2025-11-17 21:48:03 +0100

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){var chat = document.getElementById('chatbtn'); if(chat){ chat.addEventListener('click', function(e){ e.preventDefault(); window.open('https://wa.me/2349084742776','_blank'); }); }});
